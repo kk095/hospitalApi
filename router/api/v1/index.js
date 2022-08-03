03d52423fe5b1ea463646f00b049ec531feccf8b
krishan kant sharma <kk41110002@gmail.com>
2022-08-03 17:22:29 +0530

    d

--- router/api/v1/index.js
+++ router/api/v1/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
-// const controller = require(".../controller/api ");
 
-router.use("/products", require("./products"));
 router.use("/doctor", require("./doctor"));
 router.use("/patient", require("./patient"));
 
--- router/api/v1/user.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../../../controller/api/v1/user_controller");
-
-// router.post("/create", controller.createDoctor);
-// router.post("/createtoken", controller.createToken);
-
-module.exports = router;
